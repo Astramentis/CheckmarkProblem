143d2d7198436119d2bc0221e9217f0b4bcbf23b
Astramentis <WesleyFLVS@gmail.com>
2023-07-13 17:57:41 -0400

    Project stalled
    Tweet analysis remains stalled until there's a viable way to pull tweets without paying $100 a month to Elon.

--- twitter.py
+++ twitter.py
@@ -1,12 +1,17 @@
 #pip install tweepy
 #pip install OpenAI
-
+from tools import Timer
 import sqlite3
 import openai
 import key
 import string
-openai.api_key = key.key
-
+import tweepy
+import ssl
+import csv
+openai.api_key = key.AIkey
+auth = tweepy.OAuthHandler(key.twe_key , key.twe_key_secret)
+auth.set_access_token(key.twe_token , key.twe_token_secret)
+api = tweepy.API(auth)
 
 def sentiment_analysis(text):
     MODEL = "gpt-3.5-turbo"
@@ -27,10 +32,34 @@ def sentiment_analysis(text):
         sentiment = "2"
     else:
         sentiment = "0"
-    #t.sleep() #ratelimit 
+    #ratelimit here
     return sentiment  
 
+def tweet_pull(name,tweet_id):
+    auth = tweepy.OAuthHandler(key.twe_key , key.twe_key_secret)
+    auth.set_access_token(key.twe_token , key.twe_token_secret)
+    api = tweepy.API(auth)
+    ssl._create_default_https_context = ssl._create_unverified_context
+    replies = []
+    for tweet in tweepy.Cursor(api.search_tweets,q='to:'+name,result_type='recent',timeout=999999).items(1000):
+        print('here')
+        if hasattr(tweet, 'in_reply_to_tweet_id'):
+            if (tweet.in_reply_to_tweet_id==tweet_id):
+                print(tweet)
+                replies.append(tweet)
 
-sentiment = sentiment_analysis("TESTING TESTING 123")
-print(sentiment)
-print("finished")
+
+    with open('replies_clean.csv', 'wb') as f:
+        csv_writer = csv.DictWriter(f, fieldnames=('user', 'text'))
+        csv_writer.writeheader()
+        for tweet in replies:
+            row = {'user': tweet.user.screen_name, 'text': tweet.text.encode('ascii', 'ignore').replace('\n', ' ')}
+            csv_writer.writerow(row)            
+
+tweet_pull('NateSilver538','1676980499410202624')
+
+
+#sentiment = sentiment_analysis("TESTING TESTING 123")
+#print(sentiment)
+#print("finished")
+
