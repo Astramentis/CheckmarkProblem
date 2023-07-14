ada43b282fb67340b4c8f1764e7062fe6e4626f1
Astramentis <WesleyFLVS@gmail.com>
2023-07-14 15:50:44 -0400

    removing twitter junk

--- twitter.py
+++ twitter.py
@@ -3,13 +3,7 @@ import sqlite3
 import openai
 import key
 import string
-import tweepy
-import ssl
-import csv
 openai.api_key = key.AIkey
-auth = tweepy.OAuthHandler(key.twe_key , key.twe_key_secret)
-auth.set_access_token(key.twe_token , key.twe_token_secret)
-api = tweepy.API(auth)
 
 def sentiment_analysis(text):
     MODEL = "gpt-3.5-turbo"
@@ -32,30 +26,6 @@ def sentiment_analysis(text):
         sentiment = "0"
     #ratelimit here
     return sentiment  
-
-def tweet_pull(name,tweet_id):
-    auth = tweepy.OAuthHandler(key.twe_key , key.twe_key_secret)
-    auth.set_access_token(key.twe_token , key.twe_token_secret)
-    api = tweepy.API(auth)
-    ssl._create_default_https_context = ssl._create_unverified_context
-    replies = []
-    for tweet in tweepy.Cursor(api.search_tweets,q='to:'+name,result_type='recent',timeout=999999).items(1000):
-        print('here')
-        if hasattr(tweet, 'in_reply_to_tweet_id'):
-            if (tweet.in_reply_to_tweet_id==tweet_id):
-                print(tweet)
-                replies.append(tweet)
-
-
-    with open('replies_clean.csv', 'wb') as f:
-        csv_writer = csv.DictWriter(f, fieldnames=('user', 'text'))
-        csv_writer.writeheader()
-        for tweet in replies:
-            row = {'user': tweet.user.screen_name, 'text': tweet.text.encode('ascii', 'ignore').replace('\n', ' ')}
-            csv_writer.writerow(row)            
-
-tweet_pull('NateSilver538','1676980499410202624')
-
 
 #sentiment = sentiment_analysis("TESTING TESTING 123")
 #print(sentiment)
