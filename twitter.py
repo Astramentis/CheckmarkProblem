0d825f29d7d27f0f63d7598f180aa4656f40b5c5
Astramentis <WesleyFLVS@gmail.com>
2023-07-14 23:59:24 -0400

    Working Assessment function

--- twitter.py
+++ twitter.py
@@ -1,12 +1,12 @@
-from tools import Timer
 import sqlite3
 import openai
 import key
 import string
+import traceback
+import time
 openai.api_key = key.AIkey
-conn = sqlite3.connect('CheckMark.db')
+conn = sqlite3.connect('C:/Users/Wesley/Documents/GitHub/DataAnalysis/CheckmarkProblem/CheckMark.db')
 cursor = conn.cursor()
-t = Timer()
 
 def text_analysis(text):
     MODEL = "gpt-3.5-turbo"
@@ -22,14 +22,42 @@ def text_analysis(text):
     message = response["choices"][0]["message"]["content"].strip().lower().translate(str.maketrans('', '', string.punctuation))
     numeric = ''.join(filter(str.isdigit, message))
     #ratelimit here
+    time.sleep(3)
     return message, numeric
 
+cursor.execute("SELECT id, excerpt FROM Benchmark WHERE GPT_Score IS NULL ORDER BY id ASC LIMIT 1")
+rows = cursor.fetchall()
 
+# Perform text analysis on each excerpt and update database 
+for row in rows:
+    try:
+        id  = row[0]
+        text  = row[1]
+        assessment,score = text_analysis(text)
+        print(assessment)
+        print(score)
+        #remember to change database!
+        cursor.execute("UPDATE Benchmark SET GPT_ASSESSMENT = ?, GPT_Score = ? WHERE id = ?", (assessment, score, id))
+        conn.commit()
+    except:
+        traceback.print_exc()
+        break
+
+
+
+
+
+
+
+
+
+
+
+'''
 analysis, numeric = text_analysis("The floor was covered with snow-white canvas, not laid on smoothly, but rumpled over bumps and hillocks, like a real snow field. The numerous palms and evergreens that had decorated the room, were powdered with flour and strewn with tufts of cotton, like snow. Also diamond dust had been lightly sprinkled on them, and glittering crystal icicles hung from the branches.")
 print(analysis)
 print(numeric)
 print("finished")
-
+'''
 
 #dataset credits: https://www.kaggle.com/datasets/bwandowando/ukraine-russian-crisis-twitter-dataset-1-2-m-rows?select=0825_UkraineCombinedTweetsDeduped.csv.gzip
-
